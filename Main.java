408455f3fbc38dbed83ad4dc61464e33d1510b4e
George Stefanakis <george@Georges-MacBook-Air.local>
2026-09-24 10:39:19 -0700

    practice

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//print "Hello GitHub"
+
+		//print your name
+
+		//make any other edits to this file you want, get creative!
+	}
+}
